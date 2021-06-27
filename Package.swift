13284df5372852762aef609ff45e45605eaf99e9
Frederik Sørensen <frederik@greenerpastures.dk>
2021-06-27 21:55:30 +0200

    Added bundles

--- Package.swift
+++ Package.swift
@@ -16,6 +16,14 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
+		.target(
+			name: "GoogleCastCoreResources",
+			resources: [.copy("./GoogleCastCoreResources.bundle")]
+		),
+		.target(
+			name: "GoogleCastUIResources",
+			resources: [.copy("./GoogleCastUIResources.bundle")]
+		),
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
